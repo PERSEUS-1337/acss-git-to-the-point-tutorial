7294199a87e10908486cbc8a99f776205f3d8a13
PERSEUS-1337 <abramillano@up.edu.ph>
2023-04-29 20:54:26 +0800

    refactor: changes in encryption.py

--- encryption.py
+++ encryption.py
@@ -7,9 +7,11 @@ class AESCipher:
 
     def encrypt(self, data):
         cipher = AES.new(self.key, AES.MODE_CBC)
+        ciphertext = cipher.encrypt(pad(data, AES.block_size))
         return cipher.iv + ciphertext
 
     def decrypt(self, data):
         iv, ciphertext = data[:AES.block_size], data[AES.block_size:]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
         return plaintext
